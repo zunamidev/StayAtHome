a9bc11a9c226e1c54d41663eec696537f8b4c070
Zunami <hamza@isotoxin.com>
2020-03-20 20:45:45 +0100

    Init

---
+++ StayAtHoome/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using StayAtHoome.Services;
+using StayAtHoome.Views;
+
+namespace StayAtHoome
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
