742fec6224fc8216528e92965dbd0f1528ffe978
Restyled.io <commits@restyled.io>
2020-03-22 12:20:53 +0000

    Restyled by clang-format

--- StayAtHoome/ViewModels/MapViewModel.cs
+++ StayAtHoome/ViewModels/MapViewModel.cs
@@ -2,48 +2,39 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms.Maps;
-namespace StayAtHoome.ViewModels
-{
-class MapViewModel : BaseViewModel
-{
+namespace StayAtHoome.ViewModels {
+  class MapViewModel : BaseViewModel {
     public Position userLocation {
-        get;
-        set;
+      get;
+      set;
     }
     public Position homeLocation {
-        get;
-        set;
+      get;
+      set;
     }
 
     public Int32 distance {
-        get;
-        set;
+      get;
+      set;
     }
 
     public Map userMap {
-        get;
-        set;
+      get;
+      set;
     }
 
+    public MapViewModel() {
+      userLocation = new Position();
+      homeLocation = new Position();
 
+      userMap = new Map(MapSpan.FromCenterAndRadius(
+          new Position(49.007038, 8.397768), Distance.FromKilometers(10)));
 
+      Pin p1 = new Pin();
+      p1.Position = new Position(49.008851, 8.398506);
+      p1.Label = "Alte Bank - Gutes Restaurant, excellente Paella gegessen";
 
-    public MapViewModel()
-    {
-        userLocation = new Position();
-        homeLocation = new Position();
-
-        userMap = new Map(
-            MapSpan.FromCenterAndRadius(
-                new Position(49.007038, 8.397768),
-                Distance.FromKilometers(10)
-            ));
-
-        Pin p1 = new Pin();
-        p1.Position = new Position(49.008851, 8.398506);
-        p1.Label = "Alte Bank - Gutes Restaurant, excellente Paella gegessen";
-
-        userMap.Pins.Add(p1);
+      userMap.Pins.Add(p1);
     }
-}
+  }
 }
--- StayAtHoome/Views/Map.xaml.cs
+++ StayAtHoome/Views/Map.xaml.cs
@@ -7,21 +7,16 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace StayAtHoome.Views
-{
-[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class Map : ContentPage
-{
+namespace StayAtHoome.Views {
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class Map : ContentPage {
     ViewModels.MapViewModel viewModel;
 
+    public Map() {
+      NavigationPage.SetHasNavigationBar(this, false);
+      InitializeComponent();
 
-    public Map()
-    {
-        NavigationPage.SetHasNavigationBar(this, false);
-        InitializeComponent();
-
-        BindingContext = viewModel = new ViewModels.MapViewModel();
-
+      BindingContext = viewModel = new ViewModels.MapViewModel();
     }
-}
+  }
 }
