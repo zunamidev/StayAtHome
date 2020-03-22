aa384fcc8aecbef841ed64c4ed0df78e6691a316
Restyled.io <commits@restyled.io>
2020-03-22 12:31:54 +0000

    Restyled by clang-format

--- StayAtHoome/ViewModels/MapViewModel.cs
+++ StayAtHoome/ViewModels/MapViewModel.cs
@@ -2,46 +2,39 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms.Maps;
-namespace StayAtHoome.ViewModels
-{
-class MapViewModel : BaseViewModel
-{
-    public Position userLocation
-    {
-        get;
-        set;
+namespace StayAtHoome.ViewModels {
+  class MapViewModel : BaseViewModel {
+    public Position userLocation {
+      get;
+      set;
     }
-    public Position homeLocation
-    {
-        get;
-        set;
+    public Position homeLocation {
+      get;
+      set;
     }
 
-    public Int32 distance
-    {
-        get;
-        set;
+    public Int32 distance {
+      get;
+      set;
     }
 
-    public Map userMap
-    {
-        get;
-        set;
+    public Map userMap {
+      get;
+      set;
     }
 
-    public MapViewModel()
-    {
-        userLocation = new Position();
-        homeLocation = new Position();
+    public MapViewModel() {
+      userLocation = new Position();
+      homeLocation = new Position();
 
-        userMap = new Map(MapSpan.FromCenterAndRadius(
-                              new Position(49.007038, 8.397768), Distance.FromKilometers(10)));
+      userMap = new Map(MapSpan.FromCenterAndRadius(
+          new Position(49.007038, 8.397768), Distance.FromKilometers(10)));
 
-        Pin p1 = new Pin();
-        p1.Position = new Position(49.008851, 8.398506);
-        p1.Label = "Alte Bank - Gutes Restaurant, excellente Paella gegessen";
+      Pin p1 = new Pin();
+      p1.Position = new Position(49.008851, 8.398506);
+      p1.Label = "Alte Bank - Gutes Restaurant, excellente Paella gegessen";
 
-        userMap.Pins.Add(p1);
+      userMap.Pins.Add(p1);
     }
-}
+  }
 }
