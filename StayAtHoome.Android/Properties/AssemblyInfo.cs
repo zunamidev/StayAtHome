ba079a4b1940c549f9d63fc66a810b7ffcca6871
Pascal <60019278+reppentenner@users.noreply.github.com>
2020-03-22 13:31:46 +0100

    Update AssemblyInfo.cs

--- StayAtHoome.Android/Properties/AssemblyInfo.cs
+++ StayAtHoome.Android/Properties/AssemblyInfo.cs
@@ -6,14 +6,14 @@ using Android.App;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly : AssemblyTitle("StayAtHoome.Android")]
-[assembly : AssemblyDescription("")]
-[assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("")]
-[assembly : AssemblyProduct("StayAtHoome.Android")]
-[assembly : AssemblyCopyright("Copyright ©  2014")]
-[assembly : AssemblyTrademark("")]
-[assembly : AssemblyCulture("")]
+[assembly: AssemblyTitle("StayAtHoome.Android")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StayAtHoome.Android")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 [assembly:ComVisible(false)]
 
     // Version information for an assembly consists of the following four
@@ -27,9 +27,9 @@ using Android.App;
     // You can specify all the values or you can default the Build and Revision
     // Numbers by using the '*' as shown below: [assembly:
     // AssemblyVersion("1.0.*")]
-    [assembly : AssemblyVersion("1.0.0.0")]
-    [assembly:AssemblyFileVersion("1.0.0.0")]
+    [assembly: AssemblyVersion("1.0.0.0")]
+    [assembly: AssemblyFileVersion("1.0.0.0")]
 
     // Add some common permissions, these can be removed if not needed
-    [assembly : UsesPermission(Android.Manifest.Permission.Internet)]
-    [assembly:UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+    [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+    [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
