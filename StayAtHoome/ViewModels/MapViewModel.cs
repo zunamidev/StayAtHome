73bc7dd586b3ee1dd8e9319fbe6c626ef754fceb
reppentenner <pascal200797@gmail.com>
2020-03-22 12:00:14 +0100

    Map added.
    Location Karlsruhe: 49.007038, 8.397768
    checked if pin functionality works

---
+++ StayAtHoome/ViewModels/MapViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Maps;
+namespace StayAtHoome.ViewModels
+{
+    class MapViewModel : BaseViewModel
+    {
+        public Position userLocation { get; set; }
+        public Position homeLocation { get; set; }
+
+        public Int32 distance { get; set; }
+
+        public Map userMap { get; set; }
+
+
+
+
+        public MapViewModel()
+        {
+            userLocation = new Position(); 
+            homeLocation = new Position();
+
+            userMap = new Map(
+                MapSpan.FromCenterAndRadius(
+                    new Position(49.007038, 8.397768),
+                    Distance.FromKilometers(10)
+                ));
+
+            Pin p1 = new Pin();
+            p1.Position = new Position(49.008851, 8.398506);
+            p1.Label = "Alte Bank - Gutes Restaurant, excellente Paella gegessen";
+
+            userMap.Pins.Add(p1);
+        }
+    }
+}
