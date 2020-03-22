5048f6ea9648bf514925307506e61da42cbe4360
Restyled.io <commits@restyled.io>
2020-03-22 12:21:06 +0000

    Restyled by dotnet-format

--- StayAtHoome/Views/Map.xaml.cs
+++ StayAtHoome/Views/Map.xaml.cs
@@ -7,16 +7,19 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace StayAtHoome.Views {
-  [XamlCompilation(XamlCompilationOptions.Compile)]
-  public partial class Map : ContentPage {
-    ViewModels.MapViewModel viewModel;
+namespace StayAtHoome.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Map : ContentPage
+    {
+        ViewModels.MapViewModel viewModel;
 
-    public Map() {
-      NavigationPage.SetHasNavigationBar(this, false);
-      InitializeComponent();
+        public Map()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
 
-      BindingContext = viewModel = new ViewModels.MapViewModel();
+            BindingContext = viewModel = new ViewModels.MapViewModel();
+        }
     }
-  }
 }
